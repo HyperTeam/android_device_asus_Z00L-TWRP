31bca2686364ed626f8d8f2781b300b298e6aa41
theimpulson <aayushgupta219@gmail.com>
2019-12-01 15:53:16 +0000

    Z00L: Use Toolbox

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,6 +80,7 @@ TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXTRA_LANGUAGES := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_HAS_EDL_MODE := true
+TW_USE_TOOLBOX := true
 
 # Workaround for error copying vendor files to recovery ramdisk
 TARGET_COPY_OUT_VENDOR := system/vendor
